6de091fbc87b41b0a3f10c6b30e4c589e954e31d
Daniil Dorofeev <misterfoxkk@gmail.com>
2023-05-25 23:55:23 +0400

    project init

---
+++ SCPSpawnFixer/EventHandlers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using PlayerRoles;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+
+namespace SCPSpawnFixer
+{
+    public class EventHandlers
+    {
+        private static List<String> _players = new();
+        
+        [PluginEvent(ServerEventType.PlayerSpawn)]
+        public void OnPlayerSpawn(Player player, RoleTypeId role)
+        {
+            if (player.Role.GetTeam() != Team.SCPs || player.Role == RoleTypeId.Scp0492 || _players.Contains(player.UserId))
+                return;
+            
+            _players.Add(player.UserId);
+            
+            Log.Info($"{player.Nickname} has spawned as SCP! Respawning it.");
+            player.SetRole(role, RoleChangeReason.None);
+        }
+        
+        [PluginEvent(ServerEventType.RoundRestart)]
+        public void OnRoundRestart()
+        {
+            _players.Clear();
+        }
+    }
+}
---
+++ SCPSpawnFixer/SCPSpawnFixer.cs
@@ -0,0 +1,19 @@
+﻿namespace SCPSpawnFixer
+{
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
+    using PluginAPI.Events;
+    
+    public class SCPSpawnFixer
+    {
+        public static SCPSpawnFixer Instance { get; private set; }
+
+        [PluginPriority(LoadPriority.Highest)]
+        [PluginEntryPoint("SCPSpawnFixer", "1.0.0", "A plugin that fixes scp spawn blackout.", "VORTEX")]
+        void LoadPlugin()
+        {
+            Instance = this;
+            EventManager.RegisterEvents<EventHandlers>(this);
+        }
+    }
+}
