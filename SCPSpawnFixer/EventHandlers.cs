ce129826cded28913d7b83d71c457b665a7c3208
Daniil Dorofeev <misterfoxkk@gmail.com>
2023-05-30 22:57:55 +0400

    change spawning condition to all classes

--- SCPSpawnFixer/EventHandlers.cs
+++ SCPSpawnFixer/EventHandlers.cs
@@ -11,16 +11,17 @@ namespace SCPSpawnFixer
     public class EventHandlers
     {
         private static List<String> _players = new();
+        private bool _isRoundStarted;
         
         [PluginEvent(ServerEventType.PlayerSpawn)]
         public void OnPlayerSpawn(Player player, RoleTypeId role)
         {
-            if (player.Role.GetTeam() != Team.SCPs || player.Role == RoleTypeId.Scp0492 || _players.Contains(player.UserId))
+            if (player.Role == RoleTypeId.Scp0492 || _players.Contains(player.UserId) || _isRoundStarted)
                 return;
             
             _players.Add(player.UserId);
             
-            Log.Info($"{player.Nickname} has spawned as SCP! Respawning it.");
+            Log.Info($"{player.Nickname} has spawned! Respawning it.");
             Timing.CallDelayed(0.1f, () => player.SetRole(role, RoleChangeReason.None));
         }
         
@@ -28,6 +29,13 @@ namespace SCPSpawnFixer
         public void OnRoundRestart()
         {
             _players.Clear();
+            _isRoundStarted = false;
+        }
+
+        [PluginEvent(ServerEventType.RoundStart)]
+        public void OnRoundStart()
+        {
+            Timing.CallDelayed(30f, () => _isRoundStarted = true);
         }
     }
 }
